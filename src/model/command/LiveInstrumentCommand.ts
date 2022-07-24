36bd8621c0711ddb5ec7635f365c1522525598c6
Brandon Fergerson <bfergerson@apache.org>
2022-07-24 13:06:51 +0400

    temp fix for circular dependency

--- src/model/command/LiveInstrumentCommand.ts
+++ src/model/command/LiveInstrumentCommand.ts
@@ -1,6 +1,7 @@
 import LiveInstrument from "../LiveInstrument";
 import LiveSourceLocation from "../LiveSourceLocation";
 import CommandType from "./CommandType";
+import LiveInstrumentUtil from "../LiveInstrumentUtil";
 
 export default class LiveInstrumentCommand {
     commandType: CommandType
@@ -10,7 +11,7 @@ export default class LiveInstrumentCommand {
     static fromJson(json: any): LiveInstrumentCommand {
         const command = new LiveInstrumentCommand();
         command.commandType = CommandType[json.commandType];
-        command.instruments = json.instruments.map(instrument => LiveInstrument.fromJson(instrument));
+        command.instruments = json.instruments.map(instrument => LiveInstrumentUtil.fromJson(instrument));
         command.locations = json.locations.map(location => location as LiveSourceLocation);
         return command;
     }
