83fd8ef3de7b6e8060bea80d70b2276bebcdc48d
Brandon Fergerson <bfergerson@apache.org>
2022-07-25 12:31:28 +0400

    tests

--- test/test.js
+++ test/test.js
@@ -1,28 +1,70 @@
-const SYSTEM_JWT_TOKEN =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJkZXZlbG9wZXJfaWQiOiJzeXN0ZW0iLCJjcmVhdGVkX2F0IjoxNjIyNDIxMzY0ODY4" +
-    "LCJleHBpcmVzX2F0IjoxNjUzOTU3MzY0ODY4LCJpYXQiOjE2MjI0MjEzNjR9.ZVHtxQkfCF7KM_dyDOgawbwpEAsmnCWB4c8I" +
-    "52svPvVc-SlzkEe0SYrNufNPniYZeM3IF0Gbojl_DSk2KleAz9CLRO3zfegciXKeEEvGjsNOqfQjgU5yZtBWmTimVXq5QoZME" +
-    "GuAojACaf-m4J0H7o4LQNGwrDVA-noXVE0Eu84A5HxkjrRuFlQWv3fzqSRC_-lI0zRKuFGD-JkIfJ9b_wP_OjBWT6nmqkZn_J" +
-    "mK7UwniTUJjocszSA2Ma3XLx2xVPzBcz00QWyjhIyiftxNQzgqLl1XDVkRtzXUIrHnFCR8BcgR_PsqTBn5nH7aCp16zgmkkbO" +
-    "pmJXlNpDSVz9zUY4NOrB1jTzDB190COrfCXddb7JO6fmpet9_Zd3kInJx4XsT3x7JfBSWr9FBqFoUmNkgIWjkbN1TpwMyizXA" +
-    "Sp1nOmwJ64FDIbSpfpgUAqfSWXKZYhSisfnBLEyHCjMSPzVmDh949w-W1wU9q5nGFtrx6PTOxK_WKOiWU8_oeTjL0pD8pKXqJ" +
-    "MaLW-OIzfrl3kzQNuF80YT-nxmNtp5PrcxehprlPmqSB_dyTHccsO3l63d8y9hiIzfRUgUjTJbktFn5t41ADARMs_0WMpIGZJ" +
-    "yxcVssstt4J1Gj8WUFOdqPsIKigJZMn3yshC5S-KY-7S0dVd0VXgvpPqmpb9Q9Uho"
-
+const SYSTEM_JWT_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJkZXZlbG9wZXJfaWQiOiJzeXN0ZW0iLCJjcmVhdGVkX2F0IjoxNjU4NzM3Njc5NTIxLCJleHBpcmVzX2F0IjoxNjkwMjczNjc5NTIxLCJpYXQiOjE2NTg3Mzc2Nzl9.C70FjmmhLoG38x5LWa9LuzTMs1YnP0DRMfZxVuzBa6OzEp-dEvGIS5Y_k7LiZSRqjgXyaUGpXnDXzOUPigrpwj7Fx_IYrgkOwuw5l-Wv4hasI-RYQjx5MZLa-dmw_ObY8_AOHF_XNuElPVSKpKMpN6THRT2IfelXKz6OINgZr4pQzqgFlc4KnVhB87Rm9Ya-KbxvzREicp5mmVGo2Ca4_nf7SyM5ZLP1vYw4FOt_Eejioub859q-CCL1ZqwvPb3Kwmzga3USLyAzlk_R4vYZWDyZmq0qiOTBO2V97GleXbl8b4Xiw3Uxwlc76svDefNNH0VLtWM-mOPhRNnUPiUbcQHxNTCTuHF6jEhvvVbKaq5welGkINF7HLX7zGxcYwylsz6UVNa3c-LX89wfQbKlGr9pERJSwCvNtTMHq7oj_xI99e4A1cw7DX8LjAnp9zrUZgpo7OVT_TEVFXKtNQtKlKn6Pg48y6sFE3Wf48An6A5cIzrgHjfyOq1NWbDrQMon4acD_jPwcFYn21Or2YULRBRQR7hQCGBvkoIo5t24e-5ELm9h5PTcDeDLndKsik8DjzhPuLIqU9_gM0WMrr5sC0nh5eR2GMKfedcKULUU5Ql3Y_3Q0_hUx-wQ8ZT2LJez6bZF6vSgr9E6d6QpL5tfIg4vbsDYj-yqdhzB4R7XvUQ"
 const assert = require('assert');
-describe('verify-probe-connected', function () {
-    it('should return true', function () {
+const {default: axios} = require("axios");
+
+describe('Stats', function () {
+    it('/stats is accessible', function () {
         const axios = require('axios').default;
-        const bearerToken = 'Bearer ' + SYSTEM_JWT_TOKEN;
         const url = 'http://localhost:12800/stats';
         return axios.get(url, {
             headers: {
-                Authorization: bearerToken
+                Authorization: 'Bearer ' + SYSTEM_JWT_TOKEN
             }
         }).then(function (response) {
             assert.equal(response.status, 200);
         }).catch(function (error) {
-            assert.equal(error.response.status, 401);
+            assert.fail(error);
+        });
+    });
+
+    it('/stats shows probe is connected', function () {
+        const axios = require('axios').default;
+        const url = 'http://localhost:12800/stats';
+        return axios.get(url, {
+            headers: {
+                Authorization: 'Bearer ' + SYSTEM_JWT_TOKEN
+            }
+        }).then(function (response) {
+            assert.equal(
+                response.data.platform["connected-probes"],
+                1
+            )
+        }).catch(function (error) {
+            assert.fail(error);
+        });
+    });
+});
+
+
+describe('Clients', function () {
+    it('/clients is accessible', function () {
+        const axios = require('axios').default;
+        const url = 'http://localhost:12800/clients';
+        return axios.get(url, {
+            headers: {
+                Authorization: 'Bearer ' + SYSTEM_JWT_TOKEN
+            }
+        }).then(function (response) {
+            assert.equal(response.status, 200);
+        }).catch(function (error) {
+            assert.fail(error);
+        });
+    });
+
+    it('/clients contains nodejs probe', function () {
+        const axios = require('axios').default;
+        const url = 'http://localhost:12800/clients';
+        return axios.get(url, {
+            headers: {
+                Authorization: 'Bearer ' + SYSTEM_JWT_TOKEN
+            }
+        }).then(function (response) {
+            assert.equal(
+                response.data.probes[0].meta.language,
+                "nodejs"
+            )
+        }).catch(function (error) {
+            assert.fail(error);
         });
     });
 });
