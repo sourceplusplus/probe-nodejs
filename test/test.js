29cd248c0ac20b5ddd45f62a41bd462c682e4b71
Brandon Fergerson <bfergerson@apache.org>
2022-08-07 22:18:53 +0400

    test: collections

--- test/test.js
+++ test/test.js
@@ -4,3 +4,4 @@ before(TestUtils.setupProbe);
 after(TestUtils.teardownProbe);
 
 describe("test simple primitives", require("./SimplePrimitivesLiveInstrumentTest"));
+describe("test simple collections", require("./SimpleCollectionsLiveInstrumentTest"));
