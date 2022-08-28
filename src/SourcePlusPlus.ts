6b931c63ea312eaefe673c283715c478523a7947
Brandon Fergerson <bfergerson@apache.org>
2022-08-28 17:38:08 +0400

    chore: version 0.0.2

--- src/SourcePlusPlus.ts
+++ src/SourcePlusPlus.ts
@@ -130,7 +130,7 @@ namespace SourcePlusPlus {
     async function sendConnected(eventBus: EventBus): Promise<void> {
         let probeMetadata = {
             language: 'nodejs',
-            probe_version: '0.0.1', // TODO
+            probe_version: '0.0.2', // TODO
             nodejs_version: process.version,
             service: config.serviceName,
             service_instance: config.serviceInstance,
